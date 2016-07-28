8fb7aba5fa378020caf485aa01f92228dc719338
xiongdajq <wangjiaqi1995@163.com>
2016-07-28 22:21:42 +0800

    完成部分事件处理

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -18,18 +18,22 @@ bool GameScene::init() {
 	}
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
+	auto img = Sprite::create();
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-			auto img = Sprite::create("image.png");
-			auto la = Label::createWithTTF("", "fonts/Marker Felt.ttf", 24);
+			if (((i < 3 || i > 5) && (j < 3 || j > 5)) || (i > 2 && i < 6 && j > 2 && j < 6)) {
+				img = Sprite::create("image.png");
+			}
+			else {
+				img = Sprite::create("image1.png");
+			}
+			auto la = Label::createWithTTF("2", "fonts/Marker Felt.ttf", 24);
 
 			img->setPosition(Vec2(origin.x + visibleSize.width / 4 + i * 38, origin.y + visibleSize.height * 3 / 4 - j * 38));
 			la->setPosition(img->getPosition());
 			
 			this->addChild(img, 1);
-			this->addChild(img, 2);
-			la->setScale(0.5);
+			this->addChild(la, 2);
 			la->setColor(Color3B::BLUE);
 			labels labels_ = labels();
 			labels_.image = img;
@@ -40,5 +44,49 @@ bool GameScene::init() {
 		}
 	}
 
+	for (int i = 0; i < 10; i++) {
+		Button* btn = Button::create();
+		btn->setScale(2.5);
+		btn->setTitleText(to_string(i));
+		this->addChild(btn, 1);
+		btn->setPosition(Vec2(origin.x + visibleSize.width / 4 + i * 38, origin.y + visibleSize.height / 8));
+		btn->addClickEventListener(CC_CALLBACK_2(GameScene::buttonEvent, this));
+	}
+	Button* clear = Button::create();
+	clear->setScale(2.5);
+	clear->setTitleText("C");
+	clear->setPosition(Vec2(origin.x + visibleSize.width / 4 + 380, origin.y + visibleSize.height / 8));
+	this->addChild(clear, 1);
+	schedule(schedule_selector(GameScene::update), 0.1f, kRepeatForever, 0);
+	addClickListener();
 	return true;
 }
+
+void GameScene::buttonEvent(Ref * pSender, ui::Widget::TouchEventType type)
+{
+	ui::Button* button = dynamic_cast<ui::Button*>(pSender);
+	string a = button->getTitleText();
+	if (type == ui::Widget::TouchEventType::ENDED) {
+		//todos
+	}
+}
+
+void GameScene::update(float f)
+{
+	//todos
+}
+
+void GameScene::addClickListener()
+{
+	//todos
+	auto dispather = Director::getInstance()->getEventDispatcher();
+	auto eventListenerMouse = EventListenerMouse::create();
+	eventListenerMouse->onMouseDown = CC_CALLBACK_2(GameScene::onMouseDown, this);
+}
+
+void GameScene::onMouseDown(Touch * touch, Event* ev)
+{
+	auto target = static_cast<Sprite*>(ev->getCurrentTarget());
+
+}
+
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "cocos2d.h"
-
+#include <vector>
+#include <ui\UIButton.h>
 using namespace std;
+using namespace cocos2d::ui;
 class GameScene : public cocos2d::Layer
 {
 public:
@@ -14,9 +16,11 @@ private:
 		int row;
 		int col;
 		int num;
+		bool canChange;
 		cocos2d::Sprite* image;
 		cocos2d::Label* label;
 	}labels;
 	std::vector<labels> map;
+	cocos2d::Vector<Button*> button;
 };
 
